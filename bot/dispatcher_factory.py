4ff10b31b729cc5a16b9a5ba7aac4d8674993a7c
Dimash Janibekov <99655528+DizzyZ7@users.noreply.github.com>
2026-06-23 11:32:10 +0300

    Remove unused dispatcher helper

--- bot/dispatcher_factory.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-from aiogram import Dispatcher
-
-
-def configure_dispatcher_data(dispatcher: Dispatcher, **data: object) -> None:
-    for key, value in data.items():
-        dispatcher[key] = value
